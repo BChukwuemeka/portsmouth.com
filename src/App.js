99a43f012f30f1d773303f3a5ea9e85b260ea847
Chukwuemeka <emeka4u123000@gmail.com>
2023-04-24 15:28:27 +0100

    trainers removed

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Gallery from "./pages/Gallery/Gallery";
 import Home from "./pages/Home/Home";
 import NotFound from "./pages/NotFound/NotFound";
 import Plans from "./pages/Plans/Plans";
-import Trainers from "./pages/Trainers/Trainers";
 import Footer from "./components/Footer/Footer";
 import Navbar from './components/Navbar';
 
@@ -22,7 +21,6 @@ function App() {
             <Route path='contact' element={<Contact/>} />
             <Route path='gallery' element={<Gallery/>} />
             <Route path='plans' element={<Plans/>} />
-            <Route path='Trainers' element={<Trainers/>} />
             <Route path='*' element={<NotFound/>} />
           </Routes>
         <Footer />
